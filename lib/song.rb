be10eaac56f752ba6d098456f6137125b42423e3
Laura Berge <lauraeberge@gmail.com>
2019-09-13 18:51:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    song.artist_name = (filename.split(" - ")[0])
+    artist_name = (filename.split(" - ")[0])
     song
   end
   
